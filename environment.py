282ddde38c4d0ede4bd3440e76335cb74c0c408e
Jakub Ostrowski <124883831+Qubav@users.noreply.github.com>
2025-02-22 23:57:37 +0100

    page objects created

---
+++ environment.py
@@ -0,0 +1,1 @@
+URL = "https://todomvc.com/examples/jquery/dist/#/all"
---
+++ page_objects/base_page_objects.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import  Page
+from environment import URL
+
+class TodoPage:
+
+    def __init__(self, page: Page) -> None:
+        self.page = page
+        self.url = URL
+        self.todo_input_field = "input.new-todo"
+        self.button_active = "a[href='#/active']"
+        self.button_all = "a[href='#/all']"
+        self.button_completed = "a[href='#/completed']"
+
+    def open_todo_website(self) -> None:
+        self.page.goto(self.url)
+
+    def click_todo_input_field(self) -> None:
+        self.page.locator(self.todo_input_field).click()
+    
+    def enter_todo_name(self, todo_name: str) -> None:
+        self.page.locator(self.todo_input_field).fill(todo_name)
+    
+    def save_todo(self) -> None:
+        self.page.locator(self.todo_input_field).press(key="Enter")
+
+    def display_all_todos(self) -> None:
+        self.page.locator(self.button_all).click()
+    
+    def display_completed_todos(self) -> None:
+        self.page.locator(self.button_completed).click()
+    
+    def display_active_todos(self) -> None:
+        self.page.locator(self.button_active).click()
