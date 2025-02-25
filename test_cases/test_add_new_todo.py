a4086d681615982fe7100e3301774129c0f16c67
Jakub Ostrowski <124883831+Qubav@users.noreply.github.com>
2025-02-25 19:25:14 +0100

    changed input text to have positive test result

--- test_cases/test_add_new_todo.py
+++ test_cases/test_add_new_todo.py
@@ -12,5 +12,5 @@ def test_add_new_todo(todo_page: TodoPage) -> None:
     todo_page.click_todo_input_field()
     todo_page.enter_todo_name("text Ola")
     todo_page.save_todo()
-    todo_locator = todo_page.get_todo_locator("text Ola123")
+    todo_locator = todo_page.get_todo_locator("text Ola")
     expect(todo_locator).to_have_count(1)
