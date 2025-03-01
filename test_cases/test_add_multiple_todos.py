6a6dfd38d8fd57491866b4a450ca4766edb587fd
Jakub Ostrowski <124883831+Qubav@users.noreply.github.com>
2025-03-01 18:47:31 +0100

    added new tests to check whether first and nth added todos names are correct - not only number of todos added is tested now

--- test_cases/test_add_multiple_todos.py
+++ test_cases/test_add_multiple_todos.py
@@ -1,5 +1,5 @@
 from page_objects.base_page_objects import TodoPage
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 import pytest
 import allure
 
@@ -17,3 +17,23 @@ def test_add_multiple_todos(todo_page: TodoPage) -> None:
         todo_page.save_todo()
     todo_page.take_screen_shot("multiple todos added test")
     assert(len(todo_page.get_todos_completed_toggle_locators()) == len(TODO_NAMES))
+
+def test_check_nth_todo_name(todo_page: TodoPage) -> None:
+    todo_page.open_todo_website()
+    todo_page.click_todo_input_field()
+    for todo_name in TODO_NAMES:
+        todo_page.enter_todo_name(todo_name)
+        todo_page.save_todo()
+    todo_page.take_screen_shot("check nth todo name")
+    todo_locator = todo_page.get_todo_locator(TODO_NAMES[len(TODO_NAMES) - 1])
+    expect(todo_locator).to_have_count(1)
+
+def test_check_first_todo_name(todo_page: TodoPage) -> None:
+    todo_page.open_todo_website()
+    todo_page.click_todo_input_field()
+    for todo_name in TODO_NAMES:
+        todo_page.enter_todo_name(todo_name)
+        todo_page.save_todo()
+    todo_page.take_screen_shot("check nth todo name")
+    todo_locator = todo_page.get_todo_locator(TODO_NAMES[0])
+    expect(todo_locator).to_have_count(1)
