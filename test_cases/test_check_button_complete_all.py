03e80b60777546235cb5a6744d2543e8da219bb0
Jakub Ostrowski <124883831+Qubav@users.noreply.github.com>
2025-03-09 20:28:53 +0100

    added screenshots before clicking select all as completed

--- test_cases/test_check_button_complete_all.py
+++ test_cases/test_check_button_complete_all.py
@@ -15,9 +15,10 @@ def test_complete_all_todos(todo_page: TodoPage) -> None:
     for todo_name in TODO_NAMES:
         todo_page.enter_todo_name(todo_name)
         todo_page.save_todo()
+    todo_page.take_screen_shot("all todos completed - before")
     todo_page.get_all_todos_selected_as_completed()
     todo_page.display_completed_todos()
-    todo_page.take_screen_shot("all todos completed")
+    todo_page.take_screen_shot("all todos completed - after")
     assert(len(todo_page.get_todos_completed_toggle_locators()) == len(TODO_NAMES))
 
 def test_uncomplete_all_todos(todo_page: TodoPage) -> None:
@@ -26,10 +27,11 @@ def test_uncomplete_all_todos(todo_page: TodoPage) -> None:
     for todo_name in TODO_NAMES:
         todo_page.enter_todo_name(todo_name)
         todo_page.save_todo()
+    todo_page.take_screen_shot("all todos uncompleted - before")
     todo_page.get_all_todos_selected_as_completed()
     todo_page.get_all_todos_selected_as_completed()
     todo_page.display_active_todos()
-    todo_page.take_screen_shot("all todos uncompleted")
+    todo_page.take_screen_shot("all todos uncompleted - after")
     assert(len(todo_page.get_todos_completed_toggle_locators()) == len(TODO_NAMES))
 
 def test_complete_all_todos_with_one_todo_completed_before(todo_page: TodoPage) -> None:
