49688311ae3af9e6ebdb4e42d3562072b4c5473b
Jakub Ostrowski <124883831+Qubav@users.noreply.github.com>
2025-03-09 20:42:28 +0100

    added new test case

--- test_cases/test_check_completed_todo.py
+++ test_cases/test_check_completed_todo.py
@@ -39,3 +39,16 @@ def test_completed_todo_in_display_completed_view(todo_page: TodoPage) -> None:
     todo_page.take_screen_shot("completed todo in display completed view")
     todo_locator = todo_page.get_todo_locator("get hudrated")
     expect(todo_locator).to_have_count(1)
+
+def test_completed_todo_in_display_completed_view_twice_clicked(todo_page: TodoPage) -> None:
+    todo_page.open_todo_website()
+    todo_page.click_todo_input_field()
+    todo_page.enter_todo_name("get hudrated")
+    todo_page.save_todo()
+    todo_page.mark_todo_as_completed()
+    todo_page.display_completed_todos()
+    todo_page.take_screen_shot("completed todo in display completed view - twice completed test case - after first click")
+    todo_page.mark_todo_as_completed()
+    todo_page.take_screen_shot("completed todo in display completed view - twice completed test case - after second click")
+    todo_locator = todo_page.get_todo_locator("get hudrated")
+    expect(todo_locator).to_have_count(0)
